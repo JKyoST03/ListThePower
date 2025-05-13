7479c2124cc6eaae88935590e024f59ccd6d833d
MasterRoxas <josealbaarrufat@gmail.com>
2025-05-13 19:30:14 +0200

    Intro video fixed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -50,7 +50,8 @@
     <div>
 <!-- HEADER INTRO -->
 <div class="relative flex flex-col items-center justify-center h-screen bg-gray-600/70">
-    {{-- <video class="absolute top-0 left-0 w-full h-full object-cover" src="{{ asset('intro.mp4') }}" autoplay loop muted ></video> --}}
+    <!-- INTRO VÍDEO --> 
+    <video class="absolute top-0 left-0 w-full h-full object-cover" src="https://my.microsoftpersonalcontent.com/personal/075110fecf1a124d/_layouts/15/download.aspx?UniqueId=3eb1e1d0-f40c-4655-8b2b-ff4c8bece5f0&Translate=false&tempauth=v1e.eyJzaXRlaWQiOiJjOGMxNGE0MS1jMTM3LTQzZWYtYjg1Ni0xZTBmNGI0YzBiZDMiLCJhcHBpZCI6IjAwMDAwMDAwLTAwMDAtMDAwMC0wMDAwLTAwMDA0ODE3MTBhNCIsImF1ZCI6IjAwMDAwMDAzLTAwMDAtMGZmMS1jZTAwLTAwMDAwMDAwMDAwMC9teS5taWNyb3NvZnRwZXJzb25hbGNvbnRlbnQuY29tQDkxODgwNDBkLTZjNjctNGM1Yi1iMTEyLTM2YTMwNGI2NmRhZCIsImV4cCI6IjE3NDcxNjAzOTcifQ.bhMSnCgtEsxEg8yLKIC2JNHjJMufk8iwMTEzEwzGhYuox8EQy0pQnJAEKyxzIrqy3PQjP5SlgtMpqM0sX4BOI2vdJ2aIcRl2YaluB6wRlngeKrGFXnNv_8O7xiHZ-LM1TYuka3XcyGNAobIU9AcCFzgbPClPBtdQlekhSum371luSVweAHTxhnkI3sfikmHHssidvvhUIyyPNu52xCZJsLitHnAPt0ADjEImDExUTiN9Ebu3aSpsatkHmzrLI2OmxsFySfQIxC_Wwr98brSuszsxhafQ1fpxzMB0mBgzShZyaqjSiXyd4r5Oe5r6wLmEFGFWiqsawq78j9MjRLRpVHnh0Pq2XHzZL9TbOgRbtEJbr9H4ypZ_aoGvylv5QG-5-n7YYLCZBz440PfQchZOTw.-2fyJ2ADb5N2EKNKlBiV7p3TaEyuNaKRGaMHEeezrto&ApiVersion=2.0" autoplay loop muted ></video>
     <div class="relative z-10 text-center">
         <h1 class="text-7xl text-[#EDEDEC] font-bold">¿Preparado para decidir?</h1>
         <p class="text-[#EDEDEC] text-lg mt-4">Encasilla donde crees que pertenece en la escala de poder cada personaje.</p>
